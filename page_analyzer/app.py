f1cf35cceea649d0157bd87cbfa125db0d10e4dc
roman <roman.bogdanov@lanit-tercom.com>
2024-03-22 12:25:51 +0300

    added index page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 import os
@@ -9,8 +9,10 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return render_template(
+        'index.html'
+    )
 
 
 if __name__ == '__main__':
